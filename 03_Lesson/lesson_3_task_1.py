52b138135324c805ffe8329a46cbaebb07e0d4b7
Natalya1997 <natnata0897@gmail.com>
2025-10-18 18:33:43 +0300

    Добавила файлы третьего урока

---
+++ 03_Lesson/lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+
+my_user = User('Maria', 'Fedorova')
+
+print(my_user.name())
+print(my_user.suname())
+print(my_user.name_and_suname())
---
+++ 03_Lesson/smartphone.py
@@ -0,0 +1,6 @@
+class Smartphone:
+    
+    def __init__(self, mark, model, phone_number):
+        self.mark = mark
+        self.model = model
+        self.phone_number = phone_number
